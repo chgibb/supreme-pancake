1beea1d30e01267ca9b74f6b6ea3e9988e5ef22f
Chris Gibb <chris.gibb@outlook.com>
2019-01-04 10:09:32 -0500

    initial fields with default values

--- src/include/tweet.hpp
+++ src/include/tweet.hpp
@@ -17,21 +17,21 @@ namespace PanCake
     class Tweet
     {
         public:
-            std::string text;
-            std::string textHash;
-            std::string user;
-            std::string userHash;
-            std::string date;
-            std::string id;
-            std::string committed;
-            bool isRetweet;
-            bool isPinned;
-            bool isReplyTo;
-            int replyCount;
-            int reTweetCount;
-            int favouriteCount;
-            int sentimentScore;
-            float comparativeSentimentScore;
+            std::string text = "";
+            std::string textHash = "";
+            std::string user = "";
+            std::string userHash = "";
+            std::string date = "";
+            std::string id = "";
+            std::string committed = "";
+            bool isRetweet = false;
+            bool isPinned = false;
+            bool isReplyTo = false;
+            int replyCount = 0;
+            int reTweetCount = 0;
+            int favouriteCount = 0;
+            int sentimentScore = 0;
+            float comparativeSentimentScore = 0;
             std::vector<PanCake::TweetImage> images;
     };
 }
