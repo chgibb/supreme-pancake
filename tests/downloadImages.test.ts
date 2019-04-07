9256bb292b6af2a058719fd339afbcfe911a1425
Chris Gibb <chris.gibb@outlook.com>
2019-04-07 09:19:21 -0400

    lint

--- tests/downloadImages.test.ts
+++ tests/downloadImages.test.ts
@@ -1,11 +1,12 @@
 /// <reference types="jest" />
 
 import {scrapeSearchTerm} from "../src/lib/scrapeSearchTerm";
-import { downloadImages } from "../src/lib/downloadImages";
+import {downloadImages} from "../src/lib/downloadImages";
 
 jest.setTimeout(10000);
 
-it(`should download all images for tweets`,async () => {
+it("should download all images for tweets",async () => 
+{
     let res = await scrapeSearchTerm("the",20);
     expect(res.length).toBe(20);
 
