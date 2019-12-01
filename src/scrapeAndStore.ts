444131b288719e09fc1ee0562f4c9a37a362e718
chgibb <chris.gibb@outlook.com>
2019-12-01 15:33:11 -0500

    fixed linter

--- src/scrapeAndStore.ts
+++ src/scrapeAndStore.ts
@@ -1,9 +1,10 @@
-import { scrapeSearchTerm } from "./lib/scrapeSearchTerm";
-import { analyzeScrappedTweets } from "./lib/analyzeScrappedTweet";
-import { downloadAllImages } from "./lib/downloadImages";
-import { bulkStoreTweets } from "./lib/bulkStoreTweets";
+import {scrapeSearchTerm} from "./lib/scrapeSearchTerm";
+import {analyzeScrappedTweets} from "./lib/analyzeScrappedTweet";
+import {downloadAllImages} from "./lib/downloadImages";
+import {bulkStoreTweets} from "./lib/bulkStoreTweets";
 
-(async function () {
+(async function () 
+{
     let tweets = await scrapeSearchTerm("the", 30);
     tweets = tweets.concat(await scrapeSearchTerm("of", 50));
     tweets = tweets.concat(await scrapeSearchTerm("and", 50));
